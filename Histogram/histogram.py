08570ee525fa79fbdad5409198c45b7eafa0f841
KimSangYeon-DGU <sy0814k@gmail.com>
2018-12-24 23:48:17 +0900

    Edited Histogram example

--- Histogram/histogram.py
+++ Histogram/histogram.py
@@ -4,8 +4,8 @@ import cv2
 import numpy as np
 
 
-lower = np.array([0, 135, 85], dtype = "uint8")
-upper = np.array([240, 165, 115], dtype = "uint8")
+lower = np.array([0, 0, 0], dtype = "uint8")
+upper = np.array([255, 255, 255], dtype = "uint8")
 
 def segment(img):
     new_img = img.copy()
@@ -15,6 +15,7 @@ def segment(img):
     new_img[idx] = 0
     new_img = cv2.medianBlur(new_img, 3)
     plt.imshow(new_img)
+    cv2.imwrite("yoo_seg.jpg",cv2.cvtColor(new_img, cv2.COLOR_RGB2BGR))
     plt.show()
 
 def histogram(img):
@@ -39,6 +40,6 @@ def histogram(img):
 
     plt.show()
 if __name__ =="__main__":
-    img = mpimg.imread("kim.jpg")
-    segment(img)
-    #histogram(img)
+    img = mpimg.imread("son.jpg")
+    #segment(img)
+    histogram(img)
