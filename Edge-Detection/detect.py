808bfc9c0171a548aa848c4bf2cd4987631603df
KimSangYeon-DGU <sy0814k@gmail.com>
2018-12-25 22:59:07 +0900

    Edited Edge detection example

--- Edge-Detection/detect.py
+++ Edge-Detection/detect.py
@@ -2,40 +2,34 @@ import cv2
 import numpy as np
 from scipy import signal
 
-sobel_x = np.array([[-1, 0, 1],
-                    [-2, 0, 2],
-                    [-1, 0, 1]])
+sobel_x = np.array([[+1, 0, -1],
+                    [+2, 0, -2],
+                    [+1, 0, -1]])
 
-sobel_y = np.array([[-1, -2, -1],
+sobel_y = np.array([[+1, +2, +1],
                     [ 0,  0,  0],
-                    [+1, +2, +1]])
+                    [-1, -2, -1]])
 
-scharr_x = np.array([[ -3, 0, +3],
-                     [-10, 0, +10],
-                     [ -3, 0, +3]]) 
-
-scharr_y = np.array([[ -3, -10, -3],
-                     [  0,   0,  0],
-                     [ +3, +10, +3]]) 
 
 def detect():
     img = cv2.imread("Lenna.jpg")
     img_height, img_width = img.shape[:2]
-    
-    b, g, r = cv2.split(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    grad_x_b = signal.convolve2d(b, sobel_x, boundary='symm', mode='same')
-    grad_x_g = signal.convolve2d(g, sobel_x, boundary='symm', mode='same')
-    grad_x_r = signal.convolve2d(r, sobel_x, boundary='symm', mode='same')
-    edge_x = cv2.merge((grad_x_b, grad_x_g, grad_x_r))
-    
-    grad_y_b = signal.convolve2d(b, sobel_y, boundary='symm', mode='same')
-    grad_y_g = signal.convolve2d(g, sobel_y, boundary='symm', mode='same')
-    grad_y_r = signal.convolve2d(r, sobel_y, boundary='symm', mode='same')
-    edge_y = cv2.merge((grad_y_b, grad_y_g, grad_y_r))
+    # OpenCV Sobel library
+    s_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+    s_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+    sobel_origin = np.add(s_x, s_y)
+    print(np.max(sobel_origin), np.min(sobel_origin))
+    cv2.imwrite("sobel_origin.jpg", sobel_origin)
 
-    edge = edge_x + edge_y
-    cv2.imwrite("sobel.jpg", edge)
+    # Sobel Algorithm implementation
+    grad_x = signal.convolve2d(gray, sobel_x, boundary='symm', mode='same')
+    grad_y = signal.convolve2d(gray, sobel_y, boundary='symm', mode='same')
+    edge = np.add(grad_x, grad_y)
+    cv2.imwrite("my_sobel.jpg", edge)
+    print(np.max(edge), np.min(edge))
+
 
 if __name__ == "__main__":
     detect()
